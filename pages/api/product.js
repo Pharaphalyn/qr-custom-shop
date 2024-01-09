3591bd63b007bcc0a66ee5ff768611b173101eac
Alexander Sitchikhin <108qwl@gmail.com>
2024-01-09 14:19:59 +0700

    Try-catch for product API

--- pages/api/product.js
+++ pages/api/product.js
@@ -34,53 +34,57 @@ function saveProducts(products) {
 }
 
 function handler(req, res) {
-    if (req.method === 'POST') {
-        const product = req.body && req.body.product;
-        if (!product || !product.id) {
-            return res.status(400).json({ error: 'Invalid product' });
+    try {
+        if (req.method === 'POST') {
+            const product = req.body && req.body.product;
+            if (!product || !product.id) {
+                return res.status(400).json({ error: 'Invalid product' });
+            }
+            const products = getProducts();
+            products.push(product);
+            saveProducts(products);
+            return res.status(200).json({ id: product.id });
         }
-        const products = getProducts();
-        products.push(product);
-        saveProducts(products);
-        return res.status(200).json({ id: product.id });
+        if (req.method === 'GET') {
+            const products = getProducts();
+            const id = req.query.id;
+            if (!id) {
+                return res.status(200).json({ products });
+            }
+            return res.status(200).json({product: products.find(el => el.id === +id)});
+        }
+        if (req.method === 'PUT') {
+            const products = getProducts();
+            const product = req.body && req.body.product;
+            if (!product || !product.id) {
+                return res.status(400).json({ error: 'Invalid product' });
+            }
+            const index = products.findIndex(el => el.id === +product.id);
+            if (index === -1) {
+                return res.status(400).json({ error: 'Invalid product' });
+            }
+            products[index] = product;
+            saveProducts(products);
+            return res.status(200).json({ id: +product.id });
+        }
+        if (req.method === 'DELETE') {
+            const products = getProducts();
+            const id = req.body.id;
+            if (!id) {
+                return res.status(400).json({ error: 'Invalid id' });
+            }
+            const index = products.findIndex(el => el.id === id);
+            if (index === -1) {
+                return res.status(404).json({ error: 'id not found' });
+            }
+            products.splice(index, 1);
+            saveProducts(products);
+            return res.status(200).json({ id: +id });
+        }
+        return res.status(200).send();
+    } catch (e) {
+        return res.status(500).json(e);
     }
-    if (req.method === 'GET') {
-        const products = getProducts();
-        const id = req.query.id;
-        if (!id) {
-            return res.status(200).json({ products });
-        }
-        return res.status(200).json({product: products.find(el => el.id === +id)});
-    }
-    if (req.method === 'PUT') {
-        const products = getProducts();
-        const product = req.body && req.body.product;
-        if (!product || !product.id) {
-            return res.status(400).json({ error: 'Invalid product' });
-        }
-        const index = products.findIndex(el => el.id === +product.id);
-        if (index === -1) {
-            return res.status(400).json({ error: 'Invalid product' });
-        }
-        products[index] = product;
-        saveProducts(products);
-        return res.status(200).json({ id: +product.id });
-    }
-    if (req.method === 'DELETE') {
-        const products = getProducts();
-        const id = req.body.id;
-        if (!id) {
-            return res.status(400).json({ error: 'Invalid id' });
-        }
-        const index = products.findIndex(el => el.id === id);
-        if (index === -1) {
-            return res.status(404).json({ error: 'id not found' });
-        }
-        products.splice(index, 1);
-        saveProducts(products);
-        return res.status(200).json({ id: +id });
-    }
-    return res.status(200).send();
 }
 
 module.exports = allowCors(handler);
